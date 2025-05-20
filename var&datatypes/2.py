a45a2c518995b2009a4809257e8543e2a4d4921c
Utsav Tamrakar <utsavtamrakar54321@gmail.com>
2025-05-20 14:55:39 +0545

    add in git

---
+++ var&datatypes/2.py
@@ -0,0 +1,6 @@
+name="utsav"
+name1='utsav'
+name2='''utsav'''
+print(name)
+print(name1)
+print(name2)
---
+++ var&datatypes/3.py
@@ -0,0 +1,6 @@
+age=20
+a=None
+old=False
+print(type(old))
+print(type(a))
+print(type(age))
