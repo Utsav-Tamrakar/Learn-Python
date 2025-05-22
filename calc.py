c466f6fc4b850cc812c1e9d23e5e0ab91f22973a
Utsav Tamrakar <utsavtamrakar54321@gmail.com>
2025-05-23 00:00:08 +0545

    Update

---
+++ calc.py
@@ -0,0 +1,17 @@
+num1=50
+num2=10
+##calculation 
+sum = num1 + num2
+diff = num1 - num2
+div = num1 / num2
+mul = num1 * num2
+mod = num1 % num2
+cube = num1 ** 3
+
+##display
+print(f"Sum is {sum}")
+print(f"Difference is {diff}")
+print(f"Division is {div}")
+print(f"Multiplication is {mul}")
+print(f"Modulus is {mod}")
+print(f"Cube is {cube}")
---
+++ income.py
@@ -0,0 +1,5 @@
+income=20000
+expenses=10000
+print(f"Your income is {income}")
+print(f"Your income is {expenses}")
+print(f"Your Balance is {income-expenses}")
