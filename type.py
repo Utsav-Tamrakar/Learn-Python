bfd5ad83c54f30c48e64a918ce641228f05ec5d1
Utsav Tamrakar <utsavtamrakar54321@gmail.com>
2025-05-28 16:04:21 +0545

    add

---
+++ type.py
@@ -0,0 +1,11 @@
+num1=10  #integer
+num2=5.5 #float
+name="Utsav" #string
+names = ['Utsav','Rinki','Sabnam','Sonam'] #list
+
+print(type(num1))
+print(type(num2))
+print(type(name))
+print(type(names))
+print(names[1])
+print(names[0])
