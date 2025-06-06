321dfc6b8e862a53c9bb12c55f3e540ec94161b6
Utsav Tamrakar <utsavtamrakar54321@gmail.com>
2025-06-06 23:37:27 +0545

    add

---
+++ 1)loginsu.py
@@ -0,0 +1,12 @@
+atm_user_id = 10
+atm_user_pin =0000
+
+inputid= int(input("Enter user id: "))
+inputpin= int(input("Enter pin: "))
+
+if atm_user_id == inputid and atm_user_pin == inputpin :
+  print("Login successful")
+else:
+  print("Login failed")
+  
+  
