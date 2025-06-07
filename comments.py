69af5355bf213ca7cf4d7af35059edd371b6dd2b
Utsav Tamrakar <utsavtamrakar54321@gmail.com>
2025-06-07 12:12:35 +0545

    add file

--- comments.py
+++ comments.py
@@ -1,4 +1,4 @@
 # is a comments in python which is ignored by interpreter or compiler.Mostly used to explain the code,making it more readable and maintainable
-#Use it in every program to explain which helps to revise as well
+#Use it in every program to explain which helps to revise as well helps in collaboration with a squad for projects
 #this program print hello world
 print("Hello World")
