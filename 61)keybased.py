3c72ffbc8837948d41ac4614a21be028ed296c04
Utsav Tamrakar <utsavtamrakar54321@gmail.com>
2025-06-30 20:02:00 +0545

    add

--- 61)keybased.py
+++ 61)keybased.py
@@ -7,3 +7,13 @@ printDetails(id=100,name="ram",price=1000)
 print()
 printDetails(id=101,name="shyam",price=2000)
 print()
+#another example additional parameter ID
+def printDetails(id,**details):
+  print(f"Details of {id}: ")
+  for d,v in details.items():
+    print(f"{d} is {v}")
+
+printDetails(100,name="ram",price=1000)
+print()
+printDetails(101,name="shyam",price=2000)
+print()
