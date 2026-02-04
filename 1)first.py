c4013f8cdf567348592f1a1551d7f1505caf4fe0
Utsav Tamrakar <utsavtamrakar54321@gmail.com>
2026-02-04 23:12:01 +0545

    add

--- 1)first.py
+++ 1)first.py
@@ -1,1 +1,4 @@
 print("HELLO WORLD !")
+print(5)
+print("HELLO WORLD !" , 80)
+print(5*8)
--- 2)comments.py
+++ 2)comments.py
@@ -1,5 +1,4 @@
 # is a comments in python which is ignored by interpreter or compiler.Mostly used to explain the code,making it more readable and maintainable
 #Use it in every program to explain which helps to revise as well helps in collaboration with a squad for projects
-#this program print hello world
-
-print("Hello World")
+#this program print name of a coder
+print("Utsav Tamrakar")
